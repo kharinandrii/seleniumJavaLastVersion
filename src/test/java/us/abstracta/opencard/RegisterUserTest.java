f6b7b315f56a6bad8e74c6a4ac68562c9d07b6e7
kharinandrii <andrej.kharin@gmail.com>
2020-11-22 20:17:28 +0300

    add allure anotations

--- src/test/java/us/abstracta/opencard/RegisterUserTest.java
+++ src/test/java/us/abstracta/opencard/RegisterUserTest.java
@@ -1,11 +1,18 @@
 package us.abstracta.opencard;
 
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.Test;
 
 public class RegisterUserTest extends TestBase {
+    @Epic("Testing opencard website")
+    @Feature("testing user register and login functions")
+    @Story(value = "user create new aacount and login in system")
     @Test
     public void test1() {
+
         main.openPage()
                 .openMenu()
                 .clickOnRegisterButton();
@@ -42,5 +49,17 @@ public class RegisterUserTest extends TestBase {
                 .openDesktopPage();
         desktopPage.checkTitle(params.desktopTitleText)
                 .openMacPage();
+    }
+
+    @Feature("testing user login functions with correct data")
+    @Story(value = "user create new acount and login in system")
+    @Test
+    public void testUserLogin() {
+        main.openPage()
+                .openMenu()
+                .clickOnLoginButton();
+        loginForm.fillEmail()
+                .fillPassword("demo1234")
+                .clickOnButton();
     }
 }
